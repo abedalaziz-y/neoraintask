606ecd2b97141cd00070ebc0194526f45a655221
abedalazizfrontend <148460056+abedalazizfrontend@users.noreply.github.com>
2025-01-23 04:56:36 +0300

    Update for deployment

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,14 @@ import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTH_DMOAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID ,
-    storageBucket: process.env.REACT_APP_STOREAGE_BAKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
-};
-
+    apiKey: "AIzaSyAQkEEw35wWDfNjNz6S-Q-9Dh8dMvyoNYU",
+    authDomain: "user-analytics-system-448210.firebaseapp.com",
+    projectId: "user-analytics-system-448210",
+    storageBucket: "user-analytics-system-448210.firebasestorage.app",
+    messagingSenderId: "825467496328",
+    appId: "1:825467496328:web:3e331e237adb36835dfc3d"
+  };
+  
 firebase.initializeApp(firebaseConfig);
 
 // export
